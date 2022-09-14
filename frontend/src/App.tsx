1a621dedd91390341777a54a8ba4e0069162ec38
NasirXpython <Nasir.Iqbal.Dev@gmail.com>
2022-09-14 16:30:00 +0000

    feat: Add basic React application structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
+import { blue, red } from '@mui/material/colors';
+
+// Components
+import Layout from './components/Layout';
+import Dashboard from './pages/Dashboard';
+import AnalysisResult from './pages/AnalysisResult';
+import NotFound from './pages/NotFound';
+
+// Create theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[700],
+    },
+    secondary: {
+      main: red[700],
+    },
+  },
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+    ].join(','),
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+        },
+      },
+    },
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/analysis/:taskId" element={<AnalysisResult />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
+
