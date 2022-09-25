6c0d3a212ce2eac478c4ffc157849beb7f983e53
NasirXpython <Nasir.Iqbal.Dev@gmail.com>
2022-09-25 11:15:00 +0000

    feat: Add main layout component

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import {
+  AppBar,
+  Box,
+  Container,
+  Link,
+  Toolbar,
+  Typography,
+  useTheme,
+} from '@mui/material';
+import SecurityIcon from '@mui/icons-material/Security';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const theme = useTheme();
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <AppBar position="static">
+        <Toolbar>
+          <SecurityIcon sx={{ mr: 2 }} />
+          <Link
+            component={RouterLink}
+            to="/"
+            sx={{ textDecoration: 'none', color: 'inherit' }}
+          >
+            <Typography variant="h6" component="div">
+              PhishGuard
+            </Typography>
+          </Link>
+        </Toolbar>
+      </AppBar>
+
+      <Container
+        component="main"
+        maxWidth="lg"
+        sx={{
+          mt: 4,
+          mb: 4,
+          flex: 1,
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        {children}
+      </Container>
+
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: 'auto',
+          backgroundColor: theme.palette.grey[100],
+        }}
+      >
+        <Container maxWidth="lg">
+          <Typography variant="body2" color="text.secondary" align="center">
+            PhishGuard - Email Phishing Analysis Tool © {new Date().getFullYear()}
+          </Typography>
+        </Container>
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
+
